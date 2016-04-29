97df9fe0392b8dfa742b7e33cc20164f8de64047
developerworks <developerworks@163.com>
2016-04-29 22:44:10 +0800

    Add function comments

--- lib/ecto_test/factory.ex
+++ lib/ecto_test/factory.ex
@@ -4,6 +4,10 @@ defmodule EctoTest.Factory do
   alias EctoTest.Model.ChatGroup
   alias EctoTest.Model.ChatGroupUser
 
+  @doc """
+  factory 是一个数据行的生成器, 生成一个Ecto结构, 这个结构随后可作为
+  create 函数的参数, 让 create 函数把这个结构插入到数据库
+  """
   def factory(:users) do
     %User{
       # name: "Jane Smith",
@@ -24,18 +28,27 @@ defmodule EctoTest.Factory do
     }
   end
 
+  @doc """
+  向用户数据表调入数据
+  """
   def load_users do
     for _n <- 1..100 do
       create :users
     end
   end
 
+  @doc """
+  导入组表
+  """
   def load_chat_groups do
     for _n <- 1..100 do
       create :chat_groups
     end
   end
 
+  @doc """
+  导入用户组关系表
+  """
   def load_chat_group_users do
     for _n <- 1..200 do
       create :chat_group_users
